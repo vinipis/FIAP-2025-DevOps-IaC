1216ff59ba7b2cc70c0276a669b13174649caccc
Vinicius <vinicius@senseinode.com>
2025-08-20 20:46:27 -0300

    atualização de cogio, parametros e ajustes

--- terraform_ex03_state/backend.tf
+++ terraform_ex03_state/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-seu-nome"  # Usar o nome do bucket criado pelo Terraform
-    key            = "terraform.tfstate" # Nome do arquivo de estado
+    bucket         = "vinipis999-s3-test"  # Usar o nome do bucket criado pelo Terraform
+    key            = "infra/terraform.tfstate" # Nome do arquivo de estado
     region         = "us-east-1"      # Substitua pela sua região
-    dynamodb_table = "terraform-locks" # Usar o nome da tabela DynamoDB criada pelo Terraform
+    use_lockfile = true
+    dynamodb_table = "terraform-locks-vini" # Usar o nome da tabela DynamoDB criada pelo Terraform
     encrypt        = true             # Habilitar a criptografia do State no S3
   }
 }
--- terraform_ex03_state/main.tf
+++ terraform_ex03_state/main.tf
@@ -13,11 +13,7 @@ provider "aws" {
 
 # Criar um bucket S3 para armazenar o State
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-seu-nome" # Substitua pelo nome desejado do bucket (deve ser globalmente único)
-
-  versioning {
-    enabled = true
-  }
+  bucket = "vinipis999-s3-test" # Substitua pelo nome desejado do bucket (deve ser globalmente único)
 
   # Impedir exclusão acidental do bucket
   lifecycle {
@@ -29,9 +25,16 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 # Criar uma tabela DynamoDB para o bloqueio de State
 resource "aws_dynamodb_table" "terraform_locks" {
-  name           = "terraform-locks" # Nome da tabela DynamoDB
+  name           = "terraform-locks-vini" # Nome da tabela DynamoDB
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
 
